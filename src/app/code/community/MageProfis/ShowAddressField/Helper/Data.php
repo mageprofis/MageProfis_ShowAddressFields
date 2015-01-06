6c25b1e0246764351d2360f0645ade899ce25dc3
Mathis Klooß <m.klooss@loewenstark.com>
2015-01-06 08:49:45 +0100

    initial release

---
+++ src/app/code/community/MageProfis/ShowAddressField/Helper/Data.php
@@ -0,0 +1,13 @@
+<?php
+/**
+  * MageProfis_ShowAddressField
+  *
+  * @category  MageProfis
+  * @package   MageProfis_ShowAddressField
+  * @author    Mathis Klooss <mathis.klooss@mage-profis.de>
+  * @copyright 2015 Loewenstark Web-Solution GmbH (http://www.mage-profis.de). All rights served.
+  */
+class MageProfis_ShowAddressField_Helper_Data
+extends Mage_Core_Helper_Abstract
+{
+}
---
+++ src/app/code/community/MageProfis/ShowAddressField/Model/Observer.php
@@ -0,0 +1,35 @@
+<?php
+/**
+  * MageProfis_ShowAddressField
+  *
+  * @category  MageProfis
+  * @package   MageProfis_ShowAddressField
+  * @author    Mathis Klooss <mathis.klooss@mage-profis.de>
+  * @copyright 2015 Loewenstark Web-Solution GmbH (http://www.mage-profis.de). All rights served.
+  */
+class MageProfis_ShowAddressField_Model_Observer
+{
+    
+    const XML_PATH_CUSTOMER_SHOWADDRESSFIELD = 'customer/create_account/show_address_field';
+    const VARIEN_OBJECT_SHOWADDRESSFIELD     = 'show_address_fields';
+    
+    /**
+     * @mageEvent core_block_abstract_prepare_layout_before
+     * @param Varien_Object $event
+     * @return void
+     */
+    public function eventShowAddressField($event)
+    {
+        $block = $event->getBlock();
+        /* @var $block Mage_Customer_Block_Form_Register */
+        if($block instanceof Mage_Customer_Block_Form_Register)
+        {
+            if(Mage::getStoreConfigFlag(self::XML_PATH_CUSTOMER_SHOWADDRESSFIELD))
+            {
+                $block->setData(self::VARIEN_OBJECT_SHOWADDRESSFIELD, true):
+            } else {
+                $block->setData(self::VARIEN_OBJECT_SHOWADDRESSFIELD, false):
+            }
+        }
+    }
+}
