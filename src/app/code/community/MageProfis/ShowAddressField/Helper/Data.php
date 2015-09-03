7477c276dd44bc4b48c8250b77c00d88f5ab9656
Volker Thiel <volker.thiel@mage-profis.de>
2015-09-03 10:22:25 +0200

    Added README.md, updated company info

--- src/app/code/community/MageProfis/ShowAddressField/Helper/Data.php
+++ src/app/code/community/MageProfis/ShowAddressField/Helper/Data.php
@@ -5,9 +5,8 @@
   * @category  MageProfis
   * @package   MageProfis_ShowAddressField
   * @author    Mathis Klooss <mathis.klooss@mage-profis.de>
-  * @copyright 2015 Loewenstark Web-Solution GmbH (http://www.mage-profis.de). All rights served.
+  * @copyright 2015 Mage-Profis GmbH (http://www.mage-profis.de/). All rights served.
   */
-class MageProfis_ShowAddressField_Helper_Data
-extends Mage_Core_Helper_Abstract
+class MageProfis_ShowAddressField_Helper_Data extends Mage_Core_Helper_Abstract
 {
 }
